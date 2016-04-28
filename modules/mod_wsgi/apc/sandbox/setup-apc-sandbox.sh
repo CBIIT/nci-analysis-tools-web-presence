e27efe232f76e511f0e94cf4df7f79d8e332593d
Brian Park <park-brian@users.noreply.github.com>
2016-04-28 15:01:36 -0400

    set log file name for sandbox

--- modules/mod_wsgi/apc/sandbox/setup-apc-sandbox.sh
+++ modules/mod_wsgi/apc/sandbox/setup-apc-sandbox.sh
@@ -6,7 +6,8 @@ APC_WSGI_ROOT=/analysistools-sandbox/public_html/wsgi/apc
 mod_wsgi-express setup-server $APC_ROOT/apc.wsgi \
 --port 9040 \
 --user apache \
---group apache \ 
+--group apache \
 --enable-debugger \
 --server-root $APC_WSGI_ROOT \
---working-directory $APC_ROOT/apc
+--working-directory $APC_ROOT \
+--error-log-name apc-sandbox.log
