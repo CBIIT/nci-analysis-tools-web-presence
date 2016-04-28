ec217b0963ec1dd5f73deebf027f36416e4cf031
Brian Park <park-brian@users.noreply.github.com>
2016-04-28 15:00:27 -0400

    set name of log file

--- modules/mod_wsgi/apc/setup-apc.sh
+++ modules/mod_wsgi/apc/setup-apc.sh
@@ -6,6 +6,7 @@ APC_WSGI_ROOT=/analysistools/public_html/wsgi/apc
 mod_wsgi-express setup-server $APC_ROOT/apc.wsgi \
 --port 8040 \
 --user apache \
---group apache \ 
+--group apache \
 --server-root $APC_WSGI_ROOT \
---working-directory $APC_ROOT
+--working-directory $APC_ROOT \
+--error-log-name apc.log
