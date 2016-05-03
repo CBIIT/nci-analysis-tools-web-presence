2803419b8156ae945ac4d5f64f1b3af07bca7742
Brian Park <park-brian@users.noreply.github.com>
2016-05-03 09:36:32 -0400

    Update setup-apc.sh

--- modules/mod_wsgi/apc/setup-apc.sh
+++ modules/mod_wsgi/apc/setup-apc.sh
@@ -2,9 +2,10 @@
 
 APPS_ROOT=/analysistools/public_html/apps/apc
 WSGI_ROOT=/analysistools/public_html/wsgi/apc
+APC_PORT=8040
 
 mod_wsgi-express setup-server $APPS_ROOT/apc/apc.wsgi \
---port 8040 \
+--port $APC_PORT \
 --user apache \
 --group apache \
 --server-root $WSGI_ROOT/apc \
