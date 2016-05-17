4878b3ac9fb59c07860b86972746bd4bef9539e0
park-brian <re.public@outlook.com>
2016-05-17 15:21:58 -0400

    updated build script

--- modules/mod_wsgi/apc/setup-apc.sh
+++ modules/mod_wsgi/apc/setup-apc.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/sh
 
-# Example values 
-# apc_root  = /analysistools/public_html/apps/apc
-# wsgi_root = /analysistools/public_html/wsgi/apc
-# port      = 8040
+# Example values:
+# ant 	-Dport=8040 \
+# 		-Dapc_root=/analysistools/public_html/apps/apc \
+#		-Dwsgi_root=/analysistools/public_html/wsgi/apc \
+#		replace
 
 mod_wsgi-express setup-server @apc_root@/apc.wsgi \
 --port @port@ \
