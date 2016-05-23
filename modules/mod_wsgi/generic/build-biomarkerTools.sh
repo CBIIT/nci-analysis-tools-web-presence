c85c3a718fd7017e605d44808f88636e68135e0f
Brian Park <park-brian@users.noreply.github.com>
2016-05-23 16:50:15 -0400

    Update build-biomarkerTools.sh

--- modules/mod_wsgi/generic/build-biomarkerTools.sh
+++ modules/mod_wsgi/generic/build-biomarkerTools.sh
@@ -1,1 +1,6 @@
-ant -Dname=biomarkerTools -Dport=8160 -Dapp_root=/analysistools/public_html/apps/biomarkerTools -Dwsgi_root=/analysistools/public_html/wsgi/biomarkerTools
+#!/bin/sh
+
+ant -Dname=biomarkerTools \
+-Dport=8160 \
+-Dapp_root=/analysistools/public_html/apps/biomarkerTools \
+-Dwsgi_root=/analysistools/public_html/wsgi/biomarkerTools
