923e283b7c284599c883a814b6c970838151988e
Brian Park <park-brian@users.noreply.github.com>
2023-10-24 12:03:41 -0400

    update include-html component

--- common/components/include-html.js
+++ common/components/include-html.js
@@ -2,7 +2,7 @@
  * Includes a snippet of html from a remote source.
  */
 class IncludeHtml extends HTMLElement {
-  static observedAttributes = ["src"];
+  static observedAttributes = ["src", "data"];
 
   /**
    * Creates a shadow root and attaches it to the element.
@@ -24,9 +24,56 @@ class IncludeHtml extends HTMLElement {
    */
   async refresh() {
     const sourceUrl = this.getAttribute("src");
+    const templateData = this.parseJson(this.getAttribute("data"));
     const response = await fetch(sourceUrl, { cache: "no-store" });
+    const html = response.ok ? await response.text() : "";
+    const enabled = this.getConfig(html)?.ENABLED?.toUpperCase() ?? "TRUE";
     // Disable scripts by setting innerHTML directly (as opposed to appendChild)
-    this.shadow.innerHTML = response.ok ? await response.text() : ""; 
+    this.shadow.innerHTML = enabled === "TRUE"
+      ? this.formatTemplate(html, templateData)
+      : "";
+  }
+
+  /**
+   * Parses a JSON string into an object. Returns an empty object if the string is not valid JSON.
+   * @param {String} str 
+   * @returns {any}
+   */
+  parseJson(str) {
+    try {
+      return JSON.parse(str);
+    } catch (e) {
+      return {};
+    }
+  }
+
+  /**
+   * Returns an object containing configuration keys and values, defined as <!--KEY=VALUE--> in the html.
+   * @param {string} html
+   * @returns {Record<string, string>} Config object
+   */
+  getConfig(html) {
+    const matches = html.matchAll(/<!--(.*)=(.*)-->/g);
+    const config = {};
+    for (const [match, key, value] of matches) {
+      config[key.trim()] = value.trim();
+    }
+    return config;
+  }
+
+  /**
+   * Formats a string template with the provided data.
+   * @param {string} str - Template string
+   * @param {Record<string, any} data - Data to replace
+   * @param {string} start - Start delimiter
+   * @param {string} end - End delimiter
+   * @returns 
+   */
+  formatTemplate(str, data, start = "\\{\\{", end = "\\}\\}") {
+    for (let key in data) {
+      str = str.replace(new RegExp(start + key + end, "gi"), data[key]);
+    }
+    return str;
   }
 }
 
